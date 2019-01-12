2a8641e9709e82a88e0c2f4e5c6099bc3856aae0
Simon Liu Qiang <simonqiang@gmail.com>
2019-01-13 01:11:04 +0800

    task create and index

--- server/start/routes.js
+++ server/start/routes.js
@@ -24,5 +24,10 @@ Route.group(() => {
   Route.post('projects', 'ProjectController.create').middleware('auth')
   Route.delete('projects/:id', 'ProjectController.destroy').middleware('auth')
   Route.patch('projects/:id', 'ProjectController.update').middleware('auth')
+  
+  
+  Route.get('projects/:id/tasks', 'TaskController.index').middleware('auth')
+  Route.post('projects/:id/tasks', 'TaskController.create').middleware('auth')
+
 }).prefix('api');
 
