c80c76f1b8e789423f231104b9f125217741b8ef
Simon Liu Qiang <simonqiang@gmail.com>
2019-01-13 00:25:55 +0800

    manage project

--- server/start/routes.js
+++ server/start/routes.js
@@ -16,12 +16,13 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
+Route.group(() => {
+  Route.post('auth/register', 'UserController.register')
+  Route.post('auth/login', 'UserController.login')
 
-Route.post('auth/register', ({ request }) => {
-  return {
-    message : 'Hello World'
-  }
-});
+  Route.get('projects', 'ProjectController.index').middleware('auth')
+  Route.post('projects', 'ProjectController.create').middleware('auth')
+  Route.delete('projects/:id', 'ProjectController.destroy').middleware('auth')
+  Route.patch('projects/:id', 'ProjectController.update').middleware('auth')
+}).prefix('api');
+
